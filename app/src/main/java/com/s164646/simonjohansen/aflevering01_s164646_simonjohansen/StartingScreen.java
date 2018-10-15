1aae81e08cf29e805dae69359761f8b3a8a24c80
Simon J <Neelix548@hotmail.com>
2018-10-16 00:29:15 +0200

    Created new starting screen .xml with buttons and .java with onClick listeners

--- app/src/main/java/com/s164646/simonjohansen/aflevering01_s164646_simonjohansen/StartingScreen.java
+++ app/src/main/java/com/s164646/simonjohansen/aflevering01_s164646_simonjohansen/StartingScreen.java
@@ -1,4 +1,50 @@
 package com.s164646.simonjohansen.aflevering01_s164646_simonjohansen;
 
-public class StartingScreen {
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class StartingScreen extends AppCompatActivity implements View.OnClickListener {
+
+    Button button1,button2;
+
+    //TextView;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //set default view til starting screen
+        setContentView(R.layout.activity_starting_screen);
+
+        button1 = (Button) findViewById(R.id.start_Aflevering_1);
+        button2 = (Button) findViewById(R.id.start_Aflevering_2);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+
+    }
+
+
+@Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.start_Aflevering_1:
+                Intent i = new Intent(this,Aflevering01_GetStarted.class);
+                startActivity(i);
+                Toast.makeText(this, "New activity started", Toast.LENGTH_SHORT).show();
+                break;
+
+            case R.id.start_Aflevering_2:
+                Intent e = new Intent(this,Aflevering02_Galgelej.class);
+                startActivity(e);
+                Toast.makeText(this, "New activity started", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
 }
