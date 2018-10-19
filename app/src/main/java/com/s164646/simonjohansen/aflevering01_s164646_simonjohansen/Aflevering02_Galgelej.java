ab82a2dc3d00c8cdaec72e381ca68a5ee689686f
Simon J <Neelix548@hotmail.com>
2018-10-19 12:46:34 +0200

    Added strings for guessing a letter. Hasn't been implemented. Implemented EditText and ViewTexts to display game info and input. Implemented logik. Implemented switch to change ImageView for each wrong letter. Hid buttons before game, shows when start game is pressed. hides when game is over.

--- app/src/main/java/com/s164646/simonjohansen/aflevering01_s164646_simonjohansen/Aflevering02_Galgelej.java
+++ app/src/main/java/com/s164646/simonjohansen/aflevering01_s164646_simonjohansen/Aflevering02_Galgelej.java
@@ -1,14 +1,26 @@
 package com.s164646.simonjohansen.aflevering01_s164646_simonjohansen;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
 
 public class Aflevering02_Galgelej extends AppCompatActivity implements View.OnClickListener {
 
-    Button button1,button2,button3;
+    Button button1, button2, button3;
+    TextView textView, gameInfo;
+    ImageView hangMan;
+    galgelegLogik logik = new galgelegLogik();
+    EditText letterGuess;
+    InputMethodManager inputManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,15 +28,31 @@ public class Aflevering02_Galgelej extends AppCompatActivity implements View.OnC
         //set default view til starting screen
         setContentView(R.layout.activity_galgelej);
 
+        //stackoverflow solution to hiding keyboard on buttonclick (https://stackoverflow.com/questions/3400028/close-virtual-keyboard-on-button-press)
+        inputManager = (InputMethodManager)
+                getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        hangMan=(ImageView) findViewById(R.id.hangMan);
+        hangMan.setVisibility(View.INVISIBLE);
+
+        letterGuess = (EditText) findViewById(R.id.t_letterGuess);
+        letterGuess.setVisibility(View.INVISIBLE);
+
+        textView = (TextView) findViewById(R.id.t_guessLetter);
+        textView.setVisibility(View.INVISIBLE);
+
+        gameInfo = (TextView) findViewById(R.id.t_gameInfo);
+        gameInfo.setVisibility(View.INVISIBLE);
+
         button1 = (Button) findViewById(R.id.b_play_game);
+        button1.setOnClickListener(this);
 
         button2 = (Button) findViewById(R.id.b_guess_letter);
         button2.setVisibility(View.INVISIBLE);
+        button2.setOnClickListener(this);
+
         button3 = (Button) findViewById(R.id.b_give_up);
         button3.setVisibility(View.INVISIBLE);
-
-        button1.setOnClickListener(this);
-        button2.setOnClickListener(this);
         button3.setOnClickListener(this);
 
     }
@@ -32,19 +60,51 @@ public class Aflevering02_Galgelej extends AppCompatActivity implements View.OnC
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+
             case R.id.b_play_game:
+                logik.nulstil();
+                opdaterSkærm();
                 Toast.makeText(this, "Spil Startet", Toast.LENGTH_SHORT).show();
-                galgeLeg();
-                button1.setVisibility(View.GONE);
+
+                //hides playbutton and shows game.
+                button1.setVisibility(View.INVISIBLE);
                 button2.setVisibility(View.VISIBLE);
                 button3.setVisibility(View.VISIBLE);
+                textView.setVisibility(View.VISIBLE);
+                letterGuess.setVisibility(View.VISIBLE);
+                gameInfo.setVisibility(View.VISIBLE);
+                hangMan.setVisibility(View.VISIBLE);
                 break;
+
             case R.id.b_guess_letter:
+                //hides keyboard on click.
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+
+                String bogstav = letterGuess.getText().toString();
+                if (bogstav.length() != 1) {
+                    textView.setText("Skriv præcis ét bogstav");
+                    return;
+                }
+                logik.gætBogstav(bogstav);
+                letterGuess.setText("");
+                letterGuess.setError(null);
+                opdaterSkærm();
                 break;
+
             case R.id.b_give_up:
+                //hides keyboard on click.
+                inputManager.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(),
+                        InputMethodManager.HIDE_NOT_ALWAYS);
+
+                //hides game and show playbutton
                 button1.setVisibility(View.VISIBLE);
                 button2.setVisibility(View.INVISIBLE);
                 button3.setVisibility(View.INVISIBLE);
+                textView.setVisibility(View.INVISIBLE);
+                letterGuess.setVisibility(View.INVISIBLE);
+                gameInfo.setVisibility(View.INVISIBLE);
+                hangMan.setVisibility(View.INVISIBLE);
                 break;
             default:
                 Toast.makeText(this, "Default hit", Toast.LENGTH_SHORT).show();
@@ -53,21 +113,43 @@ public class Aflevering02_Galgelej extends AppCompatActivity implements View.OnC
         }
     }
 
-    public void galgeLeg(){
-        boolean isPlaying=false;
-        galgelegLogik game = new galgelegLogik();
+    private void opdaterSkærm() {
+        gameInfo.setText("Gæt ordet: " + logik.getSynligtOrd());
+        gameInfo.append("\n\nDu har " + logik.getAntalForkerteBogstaver() + " forkerte:" + logik.getBrugteBogstaver());
 
-        game.nulstil();
-        while(isPlaying){
-
-        game.gætBogstav("A");
-
-
-        game.logStatus();
+        if (logik.erSpilletVundet()) {
+            gameInfo.append("\nDu har vundet");
         }
-
-
+        if (logik.erSpilletTabt()) {
+            gameInfo.setText("Du har tabt, ordet var : " + logik.getOrdet());
+        }
+        switch (logik.getAntalForkerteBogstaver()) {
+            case 0:
+                hangMan.setImageResource(R.drawable.galge);
+                break;
+            case 1:
+                hangMan.setImageResource(R.drawable.forkert1);
+                break;
+            case 2:
+                hangMan.setImageResource(R.drawable.forkert2);
+                break;
+            case 3:
+                hangMan.setImageResource(R.drawable.forkert3);
+                break;
+            case 4:
+                hangMan.setImageResource(R.drawable.forkert4);
+                break;
+            case 5:
+                hangMan.setImageResource(R.drawable.forkert5);
+                break;
+            case 6:
+                hangMan.setImageResource(R.drawable.forkert6);
+                break;
+            case 7:
+                Toast.makeText(this, "Du har desværre tabt", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                Toast.makeText(this, "Default hit", Toast.LENGTH_SHORT).show();
+        }
     }
-
 }
-
