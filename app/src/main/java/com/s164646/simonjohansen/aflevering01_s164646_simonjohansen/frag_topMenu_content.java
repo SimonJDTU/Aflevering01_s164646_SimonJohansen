6f853afbea5d4479bf630ac1a95b20fd88283379
Simon J <Neelix548@hotmail.com>
2018-11-11 20:45:09 +0100

    Implemented fragments into frag_galgeleg_mainMenu.

--- app/src/main/java/com/s164646/simonjohansen/aflevering01_s164646_simonjohansen/frag_topMenu_content.java
+++ app/src/main/java/com/s164646/simonjohansen/aflevering01_s164646_simonjohansen/frag_topMenu_content.java
@@ -10,12 +10,12 @@ import android.view.ViewGroup;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link frag_topMenu.OnFragmentInteractionListener} interface
+ *interface
  * to handle interaction events.
- * Use the {@link frag_topMenu#newInstance} factory method to
+ * Use the {@link frag_topMenu_content#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class frag_topMenu extends Fragment implements View.OnClickListener {
+public class frag_topMenu_content extends Fragment implements View.OnClickListener {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -25,9 +25,9 @@ public class frag_topMenu extends Fragment implements View.OnClickListener {
     private String mParam1;
     private String mParam2;
 
-    private OnFragmentInteractionListener mListener;
+    //private frag_menuContent.OnFragmentInteractionListener mListener;
 
-    public frag_topMenu() {
+    public frag_topMenu_content() {
         // Required empty public constructor
     }
 
@@ -37,11 +37,11 @@ public class frag_topMenu extends Fragment implements View.OnClickListener {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment frag_topMenu.
+     * @return A new instance of fragment frag_topMenu_content.
      */
     // TODO: Rename and change types and number of parameters
-    public static frag_topMenu newInstance(String param1, String param2) {
-        frag_topMenu fragment = new frag_topMenu();
+    public static frag_topMenu_content newInstance(String param1, String param2) {
+        frag_topMenu_content fragment = new frag_topMenu_content();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -68,7 +68,7 @@ public class frag_topMenu extends Fragment implements View.OnClickListener {
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+     //   mListener = null;
     }
 
     @Override
